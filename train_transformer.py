ab45de7902cad4db0b5b2884bb4edd3807a99180
Minsub Won <minsubwon@naver.com>
2019-08-08 16:26:20 +0900

    Update train_transformer.py

--- train_transformer.py
+++ train_transformer.py
@@ -6,26 +6,34 @@ Created on Tue Jul 30 13:57:59 2019
 """
 
 from transformer_model import Model
+import tensorflow as tf
 
+if __name__ == '__main__':
 
-emb_dim = 512          
-num_layers = 6
-num_heads = 8
-linear_key_dim = 512
-linear_value_dim = 512
-ffn_dim = 2048
-enc_max_len = 100     
-dec_max_len = 100    
-
-batch_size = 256       
-warmup_steps = 4000
-training_epochs = 30
-
-
-
-model = Model(emb_dim=emb_dim, num_layers=num_layers, num_heads=num_heads,
-              linear_key_dim=linear_key_dim, linear_value_dim=linear_value_dim,ffn_dim=ffn_dim,
-              max_enc_len=enc_max_len, max_dec_len=dec_max_len,
-              batch_size=batch_size, warmup_steps=warmup_steps)
-
-model.train(training_epochs)
+    flags = tf.app.flags
+    FLAGS = flags.FLAGS
+    
+    ## Model parameter
+    flags.DEFINE_integer('hidden_dim', 512, 'dimension of hidden nodes')
+    flags.DEFINE_integer('num_layers', 6, 'number of layers of transformer encoders')
+    flags.DEFINE_integer('num_heads', 8, 'number of heads of transformer encoders')
+    flags.DEFINE_integer('linear_key_dim', 512, 'dimension of key vector')
+    flags.DEFINE_integer('linear_value_dim', 512, 'dimension of value vector')
+    flags.DEFINE_integer('ffn_dim', 2048, 'dimension of feed forward network')
+    flags.DEFINE_integer('enc_max_len', 100, 'encoder max length')
+    flags.DEFINE_integer('dec_max_len', 100, 'decoder max length')
+    flags.DEFINE_integer('batch_size', 64, 'number of batch size')
+    flags.DEFINE_integer('warmup_steps', 4000, 'number warmup steps')
+    flags.DEFINE_integer('training_epochs', 40, 'number of training epochs')
+   
+    print('========================')
+    for key in FLAGS.__flags.keys():
+        print('{} : {}'.format(key, getattr(FLAGS, key)))
+    print('========================')
+    ## Build model
+    model = Model(FLAGS.hidden_dim, FLAGS.num_layers, FLAGS.num_heads, FLAGS.linear_key_dim, 
+                  FLAGS.linear_value_dim, FLAGS.ffn_dim, FLAGS.enc_max_len, FLAGS.dec_max_len,
+                  FLAGS.batch_size, FLAGS.warmup_steps)
+    
+    ## Train model
+    model.train(FLAGS.training_epochs)
